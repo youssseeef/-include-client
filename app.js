90ddeff9de280f7f7d9a9bc0faa70d8f475503dc
Youssef Salama <youssefhanii@hotmail.com>
2018-01-30 14:28:51 +0200

    go go go

--- app.js
+++ app.js
@@ -77,6 +77,7 @@ function executeFakeData() {
 
 parser.on('data', (data) => {
     let stringifiedBuffer = data.toString('ascii');
+    console.log(stringifiedBuffer);
     //here's the data
     let locationData = stringifiedBuffer.split(',');
     let longitude = locationData[0];
